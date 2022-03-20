dd0a463f7f8dce99e7963b658d92010d58ee371b
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2022-03-20 06:45:02 +0000

    resultアニメーション実装

--- app/routes/newsletter.tsx
+++ app/routes/newsletter.tsx
@@ -1,20 +1,35 @@
 import {
   Box,
+  BoxProps,
   Button,
   Center,
   Heading,
   HStack,
   Input,
-  useBoolean,
+  Text,
   VStack,
 } from "@chakra-ui/react";
-import { useEffect, useRef, useState, VFC } from "react";
-import { Form, LoaderFunction, useLoaderData } from "remix";
+import { motion } from "framer-motion";
+import { useEffect, useRef, VFC } from "react";
+import {
+  ActionFunction,
+  Form,
+  Link,
+  LoaderFunction,
+  useActionData,
+  useLoaderData,
+} from "remix";
 import {
   SlideDocument,
   SlideQuery,
 } from "~/graphql/graphCMS/generated";
 import { graphCMSResolver } from "~/graphql/graphCMS/resolver";
+
+// ここまで
+//
+//
+//
+// ここから
 
 export const loader: LoaderFunction = async () => {
   const { data } = await graphCMSResolver(
@@ -25,22 +40,94 @@ export const loader: LoaderFunction = async () => {
   return { slide };
 };
 
+// ここまで
+//
+//
+//
+// ここから
+
+export const action: ActionFunction = async ({
+  request,
+}) => {
+  const formData = await request.formData();
+  const email = formData.get("email");
+
+  const res = await fetch(
+    `${CONVERTKIT_ENDPOINT}/forms/${CONVERTKIT_FORM_ID}/subscribe`,
+    {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        api_key: CONVERTKIT_API,
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    },
+  );
+
+  return res.json();
+};
+
+// ここまで
+//
+//
+//
+// ここから
+
+const MotionBox = motion<BoxProps>(Box);
+
+const switchVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { delay: 0.5 },
+  },
+};
+
+// ここまで
+//
+//
+//
+// ここから
+
 const Newsletter: VFC = () => {
   const { slide } = useLoaderData<SlideQuery>();
-  const element = useRef<HTMLDivElement>(null);
-  const [length, setLength] = useState<{
-    height: number | undefined;
-    width: number | undefined;
-  }>();
-  const [flag, setFlag] = useBoolean(true);
+  const element = useRef<HTMLInputElement>(null);
+  // const [length, setLength] = useState<{
+  //   height: number | undefined;
+  //   width: number | undefined;
+  // }>();
+  // const [flag, setFlag] = useBoolean(true);
+  const actionData = useActionData();
+  const state: "idle" | "success" | "error" =
+    actionData?.subscription
+      ? "success"
+      : actionData?.error
+      ? "error"
+      : "idle";
+
+  // useEffect(() => {
+  //   setLength({
+  //     height:
+  //       element.current?.getBoundingClientRect().height,
+  //     width: element.current?.getBoundingClientRect().width,
+  //   });
+  // }, []);
 
   useEffect(() => {
-    setLength({
-      height:
-        element.current?.getBoundingClientRect().height,
-      width: element.current?.getBoundingClientRect().width,
-    });
+    element.current?.focus();
   }, []);
+
+  // ここまで
+  //
+  //
+  //
+  // ここから
 
   return (
     <Center
@@ -51,31 +138,77 @@ const Newsletter: VFC = () => {
       <Box>
         <Box
           bg={"whiteAlpha.700"}
-          p={6}
           borderRadius={10}
-          ref={element}
-          h={length?.height}
-          w={length?.width}
+          p={6}
+          position={"relative"}
         >
-          {flag ? (
-            <VStack>
-              <Form method="post">
+          <MotionBox
+            variants={switchVariants}
+            animate={
+              state === "idle" ? "visible" : "hidden"
+            }
+          >
+            <Form method="post">
+              <VStack align={"center"}>
                 <Heading size={"md"}>
                   ニュースレター
                 </Heading>
                 <HStack>
-                  <Input type={"email"} name={"email"} />
+                  <Input
+                    type={"email"}
+                    name={"email"}
+                    ref={element}
+                  />
                   <Button type="submit">更新</Button>
                 </HStack>
-              </Form>
-            </VStack>
-          ) : (
-            <Center w={"100%"} h={"100%"}>
-              <Heading>成功しました</Heading>
+              </VStack>
+            </Form>
+          </MotionBox>
+          <MotionBox
+            variants={switchVariants}
+            animate={
+              state === "success" ? "visible" : "hidden"
+            }
+            initial={false}
+            position={"absolute"}
+            top={0}
+            bottom={0}
+            left={0}
+            right={0}
+          >
+            <Center h={"100%"}>
+              <VStack>
+                <Heading>成功しました</Heading>
+                <Link to={"/"}>ホームに戻る</Link>
+              </VStack>
             </Center>
-          )}
+          </MotionBox>
+          <MotionBox
+            variants={switchVariants}
+            animate={
+              state === "error" ? "visible" : "hidden"
+            }
+            initial={false}
+            position={"absolute"}
+            top={0}
+            bottom={0}
+            left={0}
+            right={0}
+          >
+            <Center h={"100%"}>
+              <VStack>
+                <Box>
+                  <Heading size={"md"}>
+                    エラーが発生しました
+                  </Heading>
+                  <Text>{actionData?.message}</Text>
+                </Box>
+                <Link to={"/newsletter"}>やり直す</Link>
+              </VStack>
+            </Center>
+          </MotionBox>
         </Box>
-        <Button onClick={setFlag.toggle}>切り替え</Button>
+        {/* <Button onClick={setFlag.toggle}>切り替え</Button> */}
       </Box>
     </Center>
   );
